39713cfc6970f512e2aaefb115f0414fe299eeb6
Wilson Yee <wilson@chapterthree.com>
2018-09-24 20:11:10 -0700

    update tracking code

--- build/js/analytics.js
+++
@@ -1,8 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-48660082-14"></script>
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'UA-48660082-14');
